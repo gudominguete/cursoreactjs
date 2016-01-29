a85003d77a680ff3462ebb63db28c4f31c33ebbf
Gustavo Lopes <gustavo_lopes_12@hotmail.com>
2016-01-29 11:06:27 -0200

    Aula 05B

---
+++ aula-05b/jsx/hello.jsx
@@ -0,0 +1,35 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Title from './title.jsx';
+
+var TitleBox = Title.TitleBox;
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+var BodyBox = React.createClass({
+    render: function(){
+    	return(
+    		<div className="bodyBox">
+    		    <TitleBox corLetra="red">
+    		        Textoooooo
+    		    </TitleBox>
+	    		<CommentBox />
+    		
+    		</div>
+    	);
+    }
+});
+
+ReactDOM.render(
+  <BodyBox />,
+  document.getElementById('content')
+);
---
+++ aula-05b/jsx/title.jsx
@@ -0,0 +1,41 @@
+
+import React from "react";
+
+var TitleBox = React.createClass({
+	getDefaultProps() {
+		return {
+			text: "",
+			corLetra:""
+		};
+	},
+	getInitialState(){
+		return{
+			hide: false
+		}
+	},
+	trocaValor(){
+		this.setState({
+			hide:!this.state.hide
+		});
+	},
+    render: function(){
+     	return (
+     		<div className="titleBox" >
+     		{!this.state.hide ?(
+     		    <h1 >
+     		        {this.props.children}
+     		    </h1>):("")
+     		}
+     		    Valor de hide: {this.state.hide.toString()}
+     		    <button onClick={this.trocaValor} 
+     		            title="Trocar valor">
+     		            Trocar Valor
+     		    </button>
+     		</div>
+     	);
+     }
+});
+
+export default {
+	TitleBox: TitleBox
+};
